f47a8eb9fefa452d5e4365985ea10248305b16f9
Shubhankar <shubhosen98@gmail.com>
2022-04-23 17:05:17 +0530

    added products and styled products

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Amazona Webiste.
+# Amazona Website.
 
 
 # Steps/Lessons
@@ -6,3 +6,8 @@
 2. Configuring VS Code
 3. Create React app
 4. Created a Git Repository
+5. List Products
+   1. create products array
+   2. add product images
+   3. render products
+   4. style products
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
+import data from "./data";
+
+
 function App() {
+
   return (
+
     <div className="App">
 
       <header>
@@ -7,7 +12,26 @@ function App() {
       </header>
 
       <main>
-        list products
+
+        <h1 className="header-featured">Featured products</h1>
+
+        <div className="products">
+
+          {data.products.map((product) => (<div className="product" key={product.slug}> {/** key props given so that react could identify the elements*/}
+            <img src={product.image} alt={product.name}/>
+
+            <a href={`/product/${product.slug}`}>
+              <p>{product.name}</p>
+            </a>
+
+            <p>{product.description}</p>
+
+            <button>{`Add to cart`.toUpperCase()}</button>
+          </div>
+          ))}
+
+        </div>
+
       </main>
 
     </div>
