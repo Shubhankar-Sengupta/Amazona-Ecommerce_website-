e6432251629055692f9fbc98558b8167b25e324e
Shubhankar <shubhosen98@gmail.com>
2022-04-24 12:14:43 +0530

    Manage State By Reducer Hook-Video 11

--- frontend/src/components/screens/HomeScreen.js
+++ frontend/src/components/screens/HomeScreen.js
@@ -1,59 +1,96 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import axios from "axios";
+import React, { useEffect, useReducer } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+function reducer(state, action) {
+  // second option that by default is passed to the reducer is action and the first is initial state
+  switch (action.type) {
+    case 'Fetch_Request':
+      return {
+        ...state,
+        loading: true, // we can show the loading box here
+      };
+    case 'Fetch_Success':
+      return {
+        ...state,
+        loading: false,
+        products: action.payload,
+      };
+    case 'Fetch_Fail':
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
 
 function HomeScreen() {
+  const [ { loading, error, products }, dispatch ] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
 
-  const [products, setProducts] = useState([]);
-
-  // imitating componentDidMount() // -it only runs once;
+  // repilcating componentDidMount();
   useEffect(() => {
 
-    const fetchData = async() => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
-    }
+    try {
 
-    fetchData();
+      const fetchData = async () => {
+        dispatch({ type: 'Fetch_Request', loading: true });
+        const result = await axios.get('/api/products');
+        dispatch({
+          type: 'Fetch_Success',
+          loading: false,
+          payload: result.data,
+          error: '',
+        });
+      };
+        // here we call fetch data.
+        fetchData();
+    } 
 
-  }, [])
+    catch (err) {
+      dispatch({type: 'Fetch_Fail', payload: err.message});
+      }
+  }, []);
 
+  // we use JSX fragment
 
-
-// we use JSX fragment
   return (
     <>
-    <h1 className="header-featured">Featured products</h1>
+      <h1 className="header-featured">Featured products</h1>
 
-        <div className="products">
-
-          {products.map((product) => (<div className="product" key={product.slug}> {/** key props given so that react could identify the elements*/}
+      <div className="products">
+        {loading? <div>Loading....</div> : error?<div>{error}</div>: products.map((product) => (
+          <div className="product" key={product.slug}>
+            {' '}
+            {/** key props given so that react could identify the elements*/}
             <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name}/>
+              <img src={product.image} alt={product.name} />
             </Link>
-
-
             <div className="product-info">
-
               <Link to={`/product/${product.slug}`}>
                 <p>{product.name}</p>
               </Link>
 
-              <p><strong>${product.price}</strong></p>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
               <p>{product.description}</p>
 
               <button>{`Add to cart`.toUpperCase()}</button>
-
             </div>
-
           </div>
-          ))}
-
-        </div>
-
+        ))}
+      </div>
     </>
-    
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
